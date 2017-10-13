e9d3f9066c211437dff0c4308c291552f2bea8fa
TheAndyBob <TheAndyBob@me.com>
2017-10-13 17:29:52 -0400

    First drag steps

--- src/form-builder/tool-box/element-categories/element-category/element-type/element-type.js
+++ src/form-builder/tool-box/element-categories/element-category/element-type/element-type.js
@@ -1,4 +1,16 @@
+import {Draggable} from 'draggable';
 
 export class ElementTypeCustomElement {
+    static inject = [Element];
 
+    constructor(element) {
+        this._element = element;
+        this._draggable;
+    }
+    attached() {
+        this._draggable = new Draggable(this._element)
+            .on('drag:start', () => console.log('drag:start'))
+            .on('drag:move',  () => console.log('drag:move'))
+            .on('drag:stop',  () => console.log('drag:stop'));
+    }
 }
--- src/form-builder/viewspace/form-row/drop-zone/drop-zone.js
+++ src/form-builder/viewspace/form-row/drop-zone/drop-zone.js
@@ -1,4 +1,20 @@
 
-export class DropZoneCustomElement {
+import {Droppable} from 'draggable';
 
+export class DropZoneCustomElement {
+    static inject = [Element];
+
+    constructor(element) {
+        this._element = element;
+        this._droppable;
+    }
+    attached() {
+        this._droppable = new Droppable([this._element], {
+            draggable: 'li',
+            droppable: '#dropzone'
+        });
+
+        droppable.on('droppable:over', () => console.log('droppable:over'))
+        droppable.on('droppable:out', () => console.log('droppable:out'));
+    }
 }
